07434c1dd5f5265cbbb8e03c847459e0f16133fb
J.Gabriel <64971254+jgabri3l@users.noreply.github.com>
2024-06-24 20:36:18 -0300

    DesafioDioPoo
    DesafioDioPoo para formação Pottencial.

---
+++ trilha-net-poo-desafio-main/Program.cs
@@ -0,0 +1,18 @@
+﻿using DesafioPOO.Models;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Iphone meuIphone = new Iphone("123456789", "iPhone 12", "111222333444555", 64)
+        meuIphone.Ligar();
+        meuIphone.ReceberLigacao();
+        meuIphone.InstalarAplicativo("WhatsApp");
+
+        Nokia meuNokia = new Nokia("123456789", "iPhone 12", "111222333444555", 64);
+        meuNokia.Ligar();
+        meuNokia.ReceberLigacao();
+        meuIphone.InstalarAplicativo("WhatsApp");
+    }
+}
